ad6f08626ba5b98397a8d62fe2d987c76b2219bf
liuyu <liuyu12345@qq.com>
2019-01-15 17:36:39 +0800

    kdTree：最近邻近点查找算法

--- Assets/Scripts/KdTree/KdTree.cs
+++ Assets/Scripts/KdTree/KdTree.cs
@@ -21,6 +21,8 @@
 
 //////////////////////////////////////////////////////////////////////////////////////
 
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -37,32 +39,7 @@ namespace Kd_Tree
             _treeNode = buildKdTreeRecursive2(objList, 0, objList.Count); // 耗时更短，内存消耗更小
             return _treeNode;
         }
-        private KdTreeNode buildKdTreeRecursive2(List<TreeData> objList, int startIndex, int length)
-        {
-            if (length == 0)
-            {
-                return null;
-            }
-            KdTreeNode node = new KdTreeNode();
-            if (length == 1)
-            {
-                node.m_Data = objList[0];
-                node.optimalSplit = -1;
-                return node;
-            }
-            // 计算最优分割项(最大方差法)
-            int optimalSplit = GetOptimalSplitAxis(objList.GetRange(startIndex, length));
-            //根据分割项，选出中位节点，分割出左右子树
-            objList.Sort(startIndex, length, new KdTreeComparer(optimalSplit));
-            int middleIndex = startIndex + length / 2;
-            node.m_Data = objList[middleIndex];
-            node.optimalSplit = optimalSplit;
-            int leftStartIndex = startIndex;
-            node.m_LeftNode = buildKdTreeRecursive2(objList, leftStartIndex, middleIndex - leftStartIndex);
-            int rightStartIndex = middleIndex + 1;
-            node.m_RightNode = buildKdTreeRecursive2(objList, rightStartIndex, startIndex + length - rightStartIndex);
-            return node;
-        }
+        //建树方法1
         private KdTreeNode buildKdTreeRecursive(List<TreeData> objList)
         {
             if (objList.Count == 0)
@@ -92,6 +69,33 @@ namespace Kd_Tree
             node.m_RightNode = buildKdTreeRecursive(rightList);
             return node;
         }
+        //建树方法2
+        private KdTreeNode buildKdTreeRecursive2(List<TreeData> objList, int startIndex, int length)
+        {
+            if (length == 0)
+            {
+                return null;
+            }
+            KdTreeNode node = new KdTreeNode();
+            if (length == 1)
+            {
+                node.m_Data = objList[startIndex];
+                node.optimalSplit = -1;
+                return node;
+            }
+            // 计算最优分割项(最大方差法)
+            int optimalSplit = GetOptimalSplitAxis(objList.GetRange(startIndex, length));
+            //根据分割项，选出中位节点，分割出左右子树
+            objList.Sort(startIndex, length, new KdTreeComparer(optimalSplit));
+            int middleIndex = startIndex + length / 2;
+            node.m_Data = objList[middleIndex];
+            node.optimalSplit = optimalSplit;
+            int leftStartIndex = startIndex;
+            node.m_LeftNode = buildKdTreeRecursive2(objList, leftStartIndex, middleIndex - leftStartIndex);
+            int rightStartIndex = middleIndex + 1;
+            node.m_RightNode = buildKdTreeRecursive2(objList, rightStartIndex, startIndex + length - rightStartIndex);
+            return node;
+        }
         //计算最优分割项
         private int GetOptimalSplitAxis(List<TreeData> objList)
         {
@@ -113,6 +117,91 @@ namespace Kd_Tree
             }
             return optimalSplit;
         }
+        //最近邻近点
+        public KdTreeNode FindNearestNode(KdTreeNode root, TreeData target)
+        {
+            Stack<KdTreeNode> backStack = new Stack<KdTreeNode>();
+            KdTreeNode nearestNode = DFSSearch(root, target, ref backStack);
+            int minDis = nearestNode.Distance(target);
+            while(backStack.Count != 0)
+            {
+                KdTreeNode backNode = backStack.Pop();
+                int backDis = backNode.Distance(target);
+                if (backDis < minDis)
+                {
+                    nearestNode = backNode;
+                    minDis = backDis;
+                }
+                if (backNode.optimalSplit != -1)
+                {
+                    if (backNode.DisOfSplit(target) < minDis)
+                    {
+                        if (backNode.LeftOf(target) && backNode.m_RightNode != null)
+                        {
+                            DFSSearch(backNode.m_RightNode, target, ref backStack);
+                        }
+                        else if (backNode.RightOf(target) && backNode.m_LeftNode != null)
+                        {
+                            DFSSearch(backNode.m_LeftNode, target, ref backStack);
+                        }
+                    }
+                }
+            }
+            return nearestNode;
+        }
+        //深度查找
+        public KdTreeNode DFSSearch(KdTreeNode root, TreeData target, ref Stack<KdTreeNode> backStack)
+        {
+            if (backStack != null)
+            {
+                backStack.Push(root);
+            }
+            if (root.optimalSplit == -1)
+            {
+                return root;
+            }
+            if (root.LeftOf(target))
+            {
+                if (root.m_LeftNode == null)
+                {
+                    return root;
+                }
+                else
+                {
+                    return DFSSearch(root.m_LeftNode, target, ref backStack);
+                }
+            }
+            else if(root.RightOf(target))
+            {
+                if (root.m_RightNode == null)
+                {
+                    return root;
+                }
+                else
+                {
+                    return DFSSearch(root.m_RightNode, target, ref backStack);
+                }
+            }
+            else
+            {
+                if (root.Distance(target) == 0)
+                {
+                    return root;
+                }
+                if (root.m_LeftNode != null)
+                {
+                    return DFSSearch(root.m_LeftNode, target, ref backStack);
+                }
+                else if (root.m_RightNode != null)
+                {
+                    return DFSSearch(root.m_RightNode, target, ref backStack);
+                }
+                else
+                {
+                    return root;
+                }
+            }
+        }
     }
 
     public class KdTreeNode
@@ -121,11 +210,44 @@ namespace Kd_Tree
         public int optimalSplit;
         public KdTreeNode m_LeftNode;
         public KdTreeNode m_RightNode;
+        public KdTreeNode m_ParentNode;
+        public bool m_IsSelfLeft;
+
+        public bool LeftOf(TreeData data)
+        {
+            return m_Data.m_Value[optimalSplit] > data.m_Value[optimalSplit];
+        }
+        public bool RightOf(TreeData data)
+        {
+            return m_Data.m_Value[optimalSplit] < data.m_Value[optimalSplit];
+        }
+        public int Distance(TreeData data)
+        {
+            int dis = 0;
+            for (int i = 0; i < m_Data.m_Value.Count; i++)
+            {
+                dis += Math.Abs(m_Data.m_Value[i] * m_Data.m_Value[i] - data.m_Value[i] * data.m_Value[i]);
+            }
+            return dis;
+        }
+        public int DisOfSplit(TreeData data)
+        {
+            return Math.Abs(m_Data.m_Value[optimalSplit] * m_Data.m_Value[optimalSplit] - data.m_Value[optimalSplit] * data.m_Value[optimalSplit]);
+        }
     }
 
     public class TreeData
     {
         public List<int> m_Value;
+        public int Distance(TreeData data)
+        {
+            int dis = 0;
+            for (int i = 0; i < m_Value.Count; i++)
+            {
+                dis += Math.Abs(m_Value[i] * m_Value[i] - data.m_Value[i] * data.m_Value[i]);
+            }
+            return dis;
+        }
     }
 
     public class KdTreeComparer : IComparer<TreeData>
--- Assets/Scripts/KdTree/TestKdTree.cs
+++ Assets/Scripts/KdTree/TestKdTree.cs
@@ -1,28 +1,100 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using Kd_Tree;
+using UnityEngine.Profiling;
 
 public class TestKdTree : MonoBehaviour {
-
+    public int seed = 0;
+    public Transform dataParent;
+    public Transform treeParent;
     List<TreeData> data = new List<TreeData>();
 	// Use this for initialization
-	void Start () {
-
-        for (int i = 0; i < 100; i++)
+    void Awake()
+    {
+        DebugData();
+    }
+    void Start () {
+        Random.InitState(seed);
+        for (int i = 0; i < 10000; i++)
         {
             data.Add(new TreeData());
             data[i].m_Value = new List<int>();
             for (int j = 0; j < 3; j++)
             {
-                data[i].m_Value.Add(Random.Range(0, 1000));
+                data[i].m_Value.Add(Random.Range(0, 100));
             }
         }
         KdTree kdtree = new KdTree();
+        Profiler.BeginSample("buildKdTree");
         var tree = kdtree.buildKdTree(data);
-    }
-	
-	// Update is called once per frame
-	void Update () {
+        Profiler.EndSample();
+        //DebugTree(tree, treeParent);
 
+        TreeData target = new TreeData();
+        target.m_Value = new List<int>();
+        for (int i = 0; i < 3; i++)
+        {
+            target.m_Value.Add(Random.Range(0, 100));
+        }
+
+        Profiler.BeginSample("FindNearestNode");
+        var nearestNode = kdtree.FindNearestNode(tree, target);
+        Profiler.EndSample();
+
+        Profiler.BeginSample("NorFind");
+        int minDis = int.MaxValue;
+        int minIndex = 0;
+        for (int i = 0; i < data.Count; i++)
+        {
+            int dis = data[i].Distance(target);
+            if (dis < minDis)
+            {
+                minDis = dis;
+                minIndex = i;
+            }
+        }
+        Profiler.EndSample();
+
+        Vector3 targetV3 = new Vector3(target.m_Value[0], target.m_Value[1], target.m_Value[2]);
+        Vector3 norV3 = new Vector3(data[minIndex].m_Value[0], data[minIndex].m_Value[1], data[minIndex].m_Value[2]);
+        Vector3 nnV3 = new Vector3(nearestNode.m_Data.m_Value[0], nearestNode.m_Data.m_Value[1], nearestNode.m_Data.m_Value[2]);
+        GenObj(targetV3, PrimitiveType.Capsule);
+        GenObj(nnV3, PrimitiveType.Sphere);
+        GenObj(norV3, PrimitiveType.Cylinder);
+        Debug.Log("target:"+targetV3);
+        Debug.Log("norV3:"+ norV3 + " dis:" + Vector3.Distance(norV3, targetV3));
+        Debug.Log("nnV3:" + nnV3 + " dis:" + Vector3.Distance(nnV3, targetV3));
+    }
+	void DebugTree(KdTreeNode root, Transform parent)
+    {
+        var pos = root.m_Data.m_Value;
+        Vector3 v3 = new Vector3(pos[0], pos[1], pos[2]);
+        var obj = GenObj(v3, PrimitiveType.Cube, parent);
+        if (root.m_LeftNode != null)
+        {
+            DebugTree(root.m_LeftNode, obj.transform);
+        }
+        if (root.m_RightNode != null)
+        {
+            DebugTree(root.m_RightNode, obj.transform);
+        }
+    }
+    void DebugData()
+    {
+        for (int i = 0; i < data.Count; i++)
+        {
+            Vector3 v3 = new Vector3(data[i].m_Value[0], data[i].m_Value[1], data[i].m_Value[2]);
+            GenObj(v3, PrimitiveType.Cube, dataParent);
+        }
+    }
+    GameObject GenObj(Vector3 v3, PrimitiveType type, Transform parent = null)
+    {
+        var obj = GameObject.CreatePrimitive(type);
+        if (parent != null)
+        {
+            obj.transform.parent = parent;
+        }
+        obj.transform.position = v3;
+        return obj;
     }
 }
